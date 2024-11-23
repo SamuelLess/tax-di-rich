d15e236a258bb9c6c4960740d8e51da7cacced29
Max Thorn <mthorn.dev@outlook.com>
2024-11-23 20:49:31 +0100

    scenario creator

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,13 +1,17 @@
-import { Avatar, Box, Container, Flex, Text } from "@mantine/core";
+import { Avatar, Divider, Flex, Text } from "@mantine/core";
 import { useLocation } from "react-router";
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
 	const location = useLocation();
 	return (
-		<Flex h={60} p={10} align={"center"} gap={10}>
-			<Avatar src="/logo.png" size={40} />
-			<Text td="underline" size="lg">UNDER</Text>
-		</Flex>
+		<>
+			<Flex h={60} p={10} align={"center"} gap={10} className={styles.root}>
+				<Avatar src="/logo.png" size={40} />
+				<Text td="underline" size="lg">UNDER</Text>
+			</Flex>
+			<Divider opacity={0.5}/>
+		</>
 	);
 }
 
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,14 +1,60 @@
 import Map from '../Map/Map'
+import { socket } from '../socket';
+import { useState, useEffect } from 'react';
+import { type Scenario } from 'Map/scenario';
+import { Button, Center, Fieldset, Text, Slider, Group, LoadingOverlay } from '@mantine/core';
+import styles from "./Home.module.css";
 
-import {socket} from '.././socket';
+const ScenarioDialogue = (props: { 
+  run: (vhs: number, cms: number) => void,
+  loading: boolean,
+}) => {
 
-import React, {useState, useEffect} from 'react';
-import { Scenario } from 'Map/scenario';
+  const [vhs, setVhs] = useState(5);
+  const [cms, setCms] = useState(10);
+
+  return (
+    <Center mt="lg" pos="relative">
+      <LoadingOverlay visible={props.loading} />
+      <Fieldset legend="Create a scenario" style={{ width: 500 }} variant='filled'>
+        <Group mt={20} mb="xs" gap={10}>
+          <Text>Number of vehicels</Text>
+          <Text size='xl' className={styles.numberIndicator}>{vhs}</Text>
+        </Group>
+        <Slider size="lg" min={0} max={50} step={1} value={vhs} onChange={setVhs} label={null} marks={[
+          { value: 0, label: '0' },
+          { value: 25, label: '25' },
+          { value: 50, label: '50' },
+        ]}/>
+        <Group mt={40} mb="xs" gap={10}>
+          <Text>Number of customers</Text>
+          <Text size='xl' className={styles.numberIndicator}>{cms}</Text>
+        </Group>
+        <Slider size="lg" min={0} max={200} step={1} value={cms} onChange={setCms} label={null} marks={[
+          { value: 0, label: '0' },
+          { value: 100, label: '100' },
+          { value: 200, label: '200' },
+        ]}/>
+        <Button mt={50} onClick={() => props.run(vhs, cms)}>Start Scenario</Button>
+      </Fieldset>
+    </Center>
+  );
+}
+
+const ScenarioDisplay = (props: { state: Scenario, times: { [key: string]: number } }) => {
+  return (
+    <div>
+      <Map scenarioState={props.state} startRemainingTimes={props.times} />
+    </div>
+  );
+} 
+
 
 const Home = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const [isConnected, setIsConnected] = useState(socket.connected);
   const [scenarioState, setScenarioState] = useState<Scenario>();
-  const [startRemainingTimes, setStartRemainingTimes] = useState<{[key: string]: number}>();
+  const [startRemainingTimes, setStartRemainingTimes] = useState<{[key: string]: number}>({});
 
 	function startScenarios(vhs_num: number, cms_num: number, speed: number) {
     socket.emit('start_scenario', vhs_num, cms_num, speed);
@@ -16,6 +62,7 @@ const Home = () => {
 
   useEffect(() => {
     function onConnect() {
+      setIsLoading(false);
       setIsConnected(true);
     }
 
@@ -24,9 +71,9 @@ const Home = () => {
     }
 
     function updateScenario(updatedScenario) {
-      // @ts-ignore
       setScenarioState(updatedScenario["data"]);
       setStartRemainingTimes(updatedScenario["start_remaining_time"]);
+      setIsLoading(false);
       console.log(updatedScenario);
     }
 
@@ -40,20 +87,26 @@ const Home = () => {
       socket.off('connect', onConnect);
       socket.off('disconnect', onDisconnect);
       socket.off('update_scenario', updateScenario);
+      socket.disconnect();
     };
 
 
   }, []);
-  // @ts-ignore
+
+  const startScenario = (vhs: number, cms: number) => {
+    setIsLoading(true);
+    startScenarios(vhs, cms, 0.02)
+  };
+
   return (
     <div style={{width: "100%", height: "500px"}}>
-
-{ scenarioState && <Map scenarioState={scenarioState} startRemainingTimes={startRemainingTimes} /> }
-    
-      <div>Hallo Welt :)</div>
-      {isConnected ? <p>test</p> : <p>not connencted</p>}
-      <button onClick={() => startScenarios(5, 10, 0.02)}>Start Scenario</button>
-
+      {scenarioState ? <ScenarioDisplay 
+        state={scenarioState}
+        times={startRemainingTimes}
+      /> : <ScenarioDialogue 
+        run={startScenario}
+        loading={isLoading && isConnected}
+      />}
     </div>
   );
 }
