6f36b4ab0abca9ba9e8f3bc6202478af6c50bfc9
Max Thorn <mthorn.dev@outlook.com>
2024-11-23 21:28:42 +0100

    default primary color black

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,6 +9,22 @@ import { createTheme, MantineProvider } from '@mantine/core'
 
 const theme = createTheme({
   fontFamily: "Inter",
+  primaryColor: 'black',
+  colors: {
+    'black': [
+      "#000000",
+      "#1C1C1C",
+      "#383838",
+      "#555555",
+      "#717171",
+      "#8D8D8D",
+      "#AAAAAA",
+      "#C6C6C6",
+      "#E2E2E2",
+      "#FFFFFF"
+    ]    
+  },
+  primaryShade: 0
 })
 
 createRoot(document.getElementById('root')!).render(
